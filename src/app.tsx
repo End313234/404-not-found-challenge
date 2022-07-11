cccc322af2504bc2e0da177bf8b65f57ceedc038
End313234 <end313234@protonmail.com>
2022-07-11 14:03:11 -0300

    Finish challenge

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,26 @@
 export const App = () => {
-	return <div>Hello World!</div>;
+	return (
+		<div className="mx-20">
+			<header className="flex items-center h-[7rem] font-inconsolata uppercase text-2xl">
+				404 not found
+			</header>
+			<main className="flex justify-center items-center gap-20 h-[calc(100vh-10rem)]">
+				<img
+					src="./scarecrow.png"
+					alt="Scare crow"
+					className="w-[540px] h-[447px]"
+				/>
+				<div className="flex flex-col items-start gap-5">
+					<h1 className="font-bold text-6xl">I have bad news for you</h1>
+					<p className="text-2xl w-96">
+						The page you are looking for might be removed or is temporarily
+						unavailable
+					</p>
+					<button className="uppercase bg-gray-800 text-white px-5 py-4">
+						back to homepage
+					</button>
+				</div>
+			</main>
+		</div>
+	);
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
+/* eslint-disable */
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ["./src/**/*.tsx"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				gray: {
+					400: "#4F4F4F",
+					800: "#333333",
+				},
+			},
+			fontFamily: {
+				"sans": "Space Mono, sans-serif",
+				"inconsolata": "Inconsolata, sans-serif"
+			}
+		},
 	},
 	plugins: [],
 };
