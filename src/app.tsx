1920edbf06383d6e7370ded85ee563e0ef8d73ed
End313234 <end313234@protonmail.com>
2022-07-11 14:54:04 -0300

    Add responsivity to the website

--- src/app.tsx
+++ src/app.tsx
@@ -1,22 +1,24 @@
 export const App = () => {
 	return (
-		<div className="mx-20">
+		<div className="mx-10 md:mx-20">
 			<header className="flex items-center h-[7rem] font-inconsolata uppercase text-2xl">
 				404 not found
 			</header>
-			<main className="flex justify-center items-center gap-20 h-[calc(100vh-10rem)]">
+			<main className="flex justify-center flex-wrap items-center gap-10 h-[calc(100vh-10rem)] md:gap-20">
 				<img
 					src="./scarecrow.png"
 					alt="Scare crow"
-					className="w-[540px] h-[447px]"
+					className="w-[286px] h-[238px] md:w-[540px] md:h-[447px]"
 				/>
-				<div className="flex flex-col items-start gap-5">
-					<h1 className="font-bold text-6xl">I have bad news for you</h1>
-					<p className="text-2xl w-96">
+				<div className="flex flex-col items-start gap-5 w-[80vw]">
+					<h1 className="font-bold text-5xl md:text-6xl">
+						I have bad news for you
+					</h1>
+					<p className="text-base md:text-2xl w-[50vw]">
 						The page you are looking for might be removed or is temporarily
 						unavailable
 					</p>
-					<button className="uppercase bg-gray-800 text-white px-5 py-4">
+					<button className="uppercase bg-gray-800 text-white px-5 py-4 mb-10">
 						back to homepage
 					</button>
 				</div>
