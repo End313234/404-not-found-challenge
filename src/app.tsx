d68aea567b7c7e692f52cd0d06b366256181b15a
End313234 <end313234@protonmail.com>
2022-07-11 15:21:59 -0300

    Improve responsivity

--- src/app.tsx
+++ src/app.tsx
@@ -4,17 +4,19 @@ export const App = () => {
 			<header className="flex items-center h-[7rem] font-inconsolata uppercase text-2xl">
 				404 not found
 			</header>
-			<main className="flex justify-center flex-wrap items-center gap-10 h-[calc(100vh-10rem)] md:gap-20">
-				<img
-					src="./scarecrow.png"
-					alt="Scare crow"
-					className="w-[286px] h-[238px] md:w-[540px] md:h-[447px]"
-				/>
-				<div className="flex flex-col items-start gap-5 w-[80vw]">
-					<h1 className="font-bold text-5xl md:text-6xl">
+			<main className="flex flex-wrap items-center gap-20 h-[calc(100vh-10rem)]">
+				<div className="flex justify-center items-center w-full md:w-auto">
+					<img
+						src="./scarecrow.png"
+						alt="Scare crow"
+						className="w-[286px] h-[238px] md:w-[500px] md:h-[460px]"
+					/>
+				</div>
+				<div className="flex flex-col items-start gap-5 md:w-[30vw]">
+					<h1 className="font-bold text-5xl md:text-6xl md:w-[40vw]">
 						I have bad news for you
 					</h1>
-					<p className="text-base md:text-2xl w-[50vw]">
+					<p className="text-base md:text-2xl w-[95%]">
 						The page you are looking for might be removed or is temporarily
 						unavailable
 					</p>
